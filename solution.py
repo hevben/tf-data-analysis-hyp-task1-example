acf3ca927d4ba0b0214c1a0db93ee9e4fadf438c
hevben <69825697+hevben@users.noreply.github.com>
2024-05-05 01:32:33 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,17 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import norm
+chat_id = 310598863 # Ваш chat ID, не меняйте название переменной
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(x_success: int, 
-             x_cnt: int, 
-             y_success: int, 
+def solution(x_success: int,
+             x_cnt: int,
+             y_success: int,
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.03
+    p1 = x_success/x_cnt
+    p2 = y_success/y_cnt
+    p = (x_success+y_success)/(x_cnt+y_cnt)
+    q = 1 - p
+    z = (p1-p2) / (np.sqrt(p*q*(1/x_cnt+1/y_cnt)))
+    pvalue = norm.cdf(z)
+    return pvalue<alpha
